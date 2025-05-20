01ae3965e5f602a802219b41bdde5f66139ef6d9
João Gabriel <joao.neves@edu.unifil.br>
2025-05-20 19:57:46 -0300

    Add files via upload

--- ArvoreBinaria.java
+++ ArvoreBinaria.java
@@ -1,37 +1,17 @@
 public class ArvoreBinaria {
-    private No raiz;
-
-    public ArvoreBinaria() {
-        this.raiz = null;
+    int contarNos(No no) {
+    if (no == null) {
+        return 0;
     }
+    return 1 + contarNos(no.esquerdo) + contarNos(no.direito);
+}
 
-    public void inserir(int valor) {
-        raiz = inserirRecursivo(raiz, valor);
-    }
-
-    private No inserirRecursivo(No atual, int valor) {
-        if (atual == null) {
-            return new No(valor);
-        }
-
-        if (valor < atual.valor) {
-            atual.esquerdo = inserirRecursivo(atual.esquerdo, valor);
-        } else if (valor > atual.valor) {
-            atual.direito = inserirRecursivo(atual.direito, valor);
-        }
-
-        return atual;
-    }
-
-    public void emOrdem() {
-        emOrdemRecursivo(raiz);
-    }
-
-    private void emOrdemRecursivo(No no) {
+    void preOrdem(No no) {
         if (no != null) {
-            emOrdemRecursivo(no.esquerdo);
             System.out.print(no.valor + " ");
-            emOrdemRecursivo(no.direito);
+            preOrdem(no.esquerdo);
+            preOrdem(no.direito);
         }
     }
+
 }
--- No.java
+++ No.java
@@ -1,11 +1,12 @@
 public class No {
-    int valor;
-    No esquerdo;
-    No direito;
 
-    public No(int valor) {
+    int valor;
+    No esquerdo, direito;
+
+    No(int valor) {
         this.valor = valor;
-        this.esquerdo = null;
-        this.direito = null;
+        esquerdo = direito = null;
     }
 }
+
+
