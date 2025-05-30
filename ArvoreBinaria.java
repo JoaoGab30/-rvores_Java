c179d9ea2751c1e0a6aca688a96ba6a32cbca841
João Gabriel <joao.neves@edu.unifil.br>
2025-05-29 21:51:11 -0300

    Método contarNóIterativo para pilha
    o método iterativo do contador de nós com fila ja estava feito, foi adicionado o método com pilha

--- ArvoreBinaria.java
+++ ArvoreBinaria.java
@@ -133,6 +133,98 @@ public class ArvoreBinaria {
         if (atual.direito != null) {
             fila.add(atual.direito);
         }
-    }
+    }   
 }
+
+public int contarNosIterativo(No raiz) {
+    if (raiz == null) {
+        return 0;
+    }
+
+    Queue<No> fila = new LinkedList<>();
+    fila.add(raiz);
+
+    int contador = 0;
+
+    while (!fila.isEmpty()) {
+        No atual = fila.poll();
+        contador++;
+
+        if (atual.esquerdo != null) {
+            fila.add(atual.esquerdo);
+        }
+        if (atual.direito != null) {
+            fila.add(atual.direito);
+        }
+    }
+
+    return contador;
+}
+    
+
+    public int contarNosIterativoPilha(No raiz) {
+        if (raiz == null) {
+            return 0;
+        }
+
+        Stack<No> pilha = new Stack<>();
+        pilha.push(raiz);
+
+        int contador = 0;
+
+        while (!pilha.isEmpty()) {
+            No atual = pilha.pop();
+            contador++;
+
+            if (atual.direito != null) {
+                pilha.push(atual.direito);
+            }
+            if (atual.esquerdo != null) {
+                pilha.push(atual.esquerdo);
+            }
+        }
+
+    }
+
+    public int contarNosFolhaRecursivo(No no) {
+    if (no == null) {
+        return 0;
+    }
+
+    if (no.esquerdo == null && no.direito == null) {
+        return 1;
+    }
+
+    return contarNosFolhaRecursivo(no.esquerdo) + contarNosFolhaRecursivo(no.direito);
+}
+    public int contarNosFolhaIterativo(No raiz) {
+    if (raiz == null) {
+        return 0;
+    }
+
+    Queue<No> fila = new LinkedList<>();
+    fila.add(raiz);
+
+    int contador = 0;
+
+    while (!fila.isEmpty()) {
+        No atual = fila.poll();
+
+        if (atual.esquerdo == null && atual.direito == null) {
+            contador++;
+        }
+
+        if (atual.esquerdo != null) {
+            fila.add(atual.esquerdo);
+        }
+
+        if (atual.direito != null) {
+            fila.add(atual.direito);
+        }
+    }
+
+    return contador;
+}
+
+
 }
